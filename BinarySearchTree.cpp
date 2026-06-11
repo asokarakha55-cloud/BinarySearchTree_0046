8da882c3a0d9c07bd84c60bd238d8c063622d48e
asokarakha55-cloud <asokarakha55@gmail.com>
2026-06-11 15:40:19 +0700

    Membuat case kedua

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -149,4 +149,9 @@ int main()
             obj.insert(word);
             break;
         }
-            }
+        case '2':
+        {
+            obj.inorder(obj.ROOT);
+            break;
+        }
+        }
