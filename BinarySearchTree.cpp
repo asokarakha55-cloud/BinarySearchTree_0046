5775e5e001bdb450f74a4e155d22dac51ff2d5cf
asokarakha55-cloud <asokarakha55@gmail.com>
2026-06-11 15:40:39 +0700

    Membuat case ke 3

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -154,4 +154,9 @@ int main()
             obj.inorder(obj.ROOT);
             break;
         }
+        case '3':
+        {
+            obj.preorder(obj.ROOT);
+            break;
         }
+                }
